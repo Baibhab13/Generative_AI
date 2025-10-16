c6218fb1ccf1ae5fab6f8deece1b9e86fb6fcb79
Baibhab13 <baibhabsahu31@gmail.com>
2025-10-16 17:36:50 +0530

    static vs dynamic prompt

--- promt_ui.py
+++ promt_ui.py
@@ -1,14 +1,46 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import streamlit as st
+from langchain_core.prompts import PromptTemplate
 
 load_dotenv()
 model = ChatGoogleGenerativeAI(model = 'gemini-2.5-flash')
 
 st.header('Research Paper Summarizer')
 
-user_input =  st.text_input("Enter you prompt")
+# Static prompt : 
+# user_input =  st.text_input("Enter you prompt")
 
-if st.button('Submit'):
-    result  =  model.invoke(user_input)
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+# template
+template = PromptTemplate(
+
+    template = """
+    Summarize the research paper titled "{paper_input}" with the following specifications:
+    Explanation Style: {style_input}
+    Explanation Length: {length_input}
+    1. Mathematical Details: 
+    - Include relevant equations and mathematical concepts.
+    - Explain the mathematical concepts using simple, intuitive code snippets where applicable.
+    2. Analogies:
+    - Use Relatable analogies to simplify compex ideas.
+    if certain information is not available, resonse with: "Insufficient information to summarize the paper." instead of guessing.
+    Ensure the summary is clear, accurate and aligns with the specified style and length.
+""",
+    input_variables = ["paper_input", "style_input", "length_input"],
+)
+
+# fill the placeholders
+prompt = template.invoke({
+    "paper_input": paper_input,
+    "style_input": style_input,
+    "length_input": length_input })
+
+if st.button('Summarize'):
+    result = model.invoke(prompt)
     st.write(result.content)
