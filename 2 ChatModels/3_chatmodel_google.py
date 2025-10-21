fd3eae6262f28e1e82533ec2b6bf7a83e167345a
Baibhab Sahu <147395059+Baibhab13@users.noreply.github.com>
2025-10-21 16:40:20 +0530

    Update API key to placeholder in chatmodel_google
    Replaced the API key with a placeholder.

--- 2 ChatModels/3_chatmodel_google.py
+++ 2 ChatModels/3_chatmodel_google.py
@@ -11,6 +11,6 @@
 
 import os
 import google.generativeai as genai
-genai.configure(api_key='AIzaSyBWOl7EF1_-EwRfQ3vSwvZXk11vjuNbDtA')
+genai.configure(api_key='YOUR_API_KEY')
 for m in genai.list_models():
     print(m.name)
