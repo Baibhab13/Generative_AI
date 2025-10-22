caac8563ce5cdabfb81d04eaca7a0251ee1c8092
Baibhab13 <ankur2002666@gmail.com>
2025-10-22 21:30:03 +0530

    removed merge conflict

--- LANGCHAIN-STRUCTURED-OUTPUT/with_structured_output_pydantic.py
+++ LANGCHAIN-STRUCTURED-OUTPUT/with_structured_output_pydantic.py
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-from langchain_google_genai import ChatGoogleGenerativeAI
-from dotenv import load_dotenv
-from typing import TypedDict, Annotated, Optional
-
-load_dotenv()
-
-#schema
-# class Review(TypedDict):
-    
-#     summary: str
-#     sentiment: str
-# 
-
-# Annotated description for better clarity in the output
-class Review(TypedDict):
-    key_themes: Annotated[str, "Key themes mentioned in the review"]
-    summary: Annotated[str, "A brief summary of the review"]
-    sentiment: Annotated[str, "Overall sentiment of the review, e.g., positive, negative, neutral"]
-    pros: Annotated[Optional[list[str]], "Positive aspects mentioned in the review"]
-    cons: Annotated[Optional[list[str]], "Negative aspects mentioned in the review"]
-    name: Annotated[Optional[str], "Name of the reviewer if mentioned"]
- 
-model = ChatGoogleGenerativeAI(model = 'gemini-2.5-flash')
-
-
-structured_model = model.with_structured_output(Review)
-
-result = structured_model.invoke("The hardware is great, but software feels bloated. There are too many pre-installed apps that I can't remove. ALso, the UI looks outdated compared to other brands. Hoping for a software update to fix this.")
-=======
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from typing import TypedDict, Annotated, Optional, Literal
@@ -64,6 +34,5 @@ S-Pen support is unique and useful
                                  
 Review by Nitish Singh
 """)
->>>>>>> ceb17f2 (completed langchain strucutred output)
 
 print(result)
