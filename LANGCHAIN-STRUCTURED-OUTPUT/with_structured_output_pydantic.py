78c37a90874296a0675177998bf6765175a19b66
Baibhab13 <ankur2002666@gmail.com>
2025-10-22 21:39:10 +0530

    change the model in the pydantic structured output

--- LANGCHAIN-STRUCTURED-OUTPUT/with_structured_output_pydantic.py
+++ LANGCHAIN-STRUCTURED-OUTPUT/with_structured_output_pydantic.py
@@ -1,11 +1,11 @@
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from typing import TypedDict, Annotated, Optional, Literal
 from pydantic import BaseModel, Field
 
 load_dotenv()
 
-model = ChatOpenAI()
+model = ChatGoogleGenerativeAI(model="gemini-1.5-pro", temperature=0)
 
 # schema
 class Review(BaseModel):
