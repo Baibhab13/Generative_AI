bbd7dad62559ee2dea0ff3e928541a38181c45a3
Baibhab13 <ankur2002666@gmail.com>
2025-10-24 23:11:53 +0530

    completed langchain-chain

--- LANGCHAIN-CHAINS/conditinal_chain.py
+++ LANGCHAIN-CHAINS/conditinal_chain.py
@@ -4,7 +4,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser, PydanticOutputParser
 from pydantic import BaseModel, Field
 from typing import Literal
-from langchain.schema.runnable import RunnableParallel, RunnableBranch
+from langchain.schema.runnable import RunnableParallel, RunnableBranch, RunnableLambda
 
 load_dotenv()
 
@@ -23,8 +23,35 @@ prompt1 = PromptTemplate(
     partial_variables={'format_instructions': parser2.get_format_instructions()}
 )
 
+prompt2 = PromptTemplate(
+    template="Write a appropriate response to the following positive feedback: \n {feedback} ",
+    input_variables=['feedback']
+)
+
+prompt3 = PromptTemplate(
+    template="Write a appropriate response to the following negative feedback: \n {feedback} ",
+    input_variables=['feedback']
+)
+
 classifier_chain = prompt1 | model | parser2
 
-result = classifier_chain.invoke({'feedback': 'The product quality is very bad and delivery was late'}).sentiment
+# result = classifier_chain.invoke({'feedback': 'The product quality is very bad and delivery was late'}).sentiment
 
-print(result)
+# print(result)
+
+# branch_chain = RunnableBranch(
+#     (condition1: chain1),
+#     (condition2: chain2)
+# )
+
+branch_chain = RunnableBranch(
+    (lambda x: x.sentiment == 'positive', prompt2 | model | parser),
+    (lambda x: x.sentiment == 'negative', prompt3 | model | parser),
+    RunnableLambda(lambda x: "could not find sentiment")
+)
+
+chain = classifier_chain | branch_chain
+
+print(chain.invoke({'feedback': 'This is a terrible phone'}))
+
+chain.get_graph().print_ascii()
