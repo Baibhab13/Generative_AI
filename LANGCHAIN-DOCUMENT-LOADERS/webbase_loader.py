d1957a9920d086708438bc05a35ade7e70d85c3d
Baibhab13 <ankur2002666@gmail.com>
2025-10-27 17:15:36 +0530

    completed Langchain-document-loader

--- LANGCHAIN-DOCUMENT-LOADERS/webbase_loader.py
+++ LANGCHAIN-DOCUMENT-LOADERS/webbase_loader.py
@@ -1,9 +1,30 @@
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.document_loaders import TextLoader
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model='gemini-2.5-flash')
+
+parser = StrOutputParser()
 
 url = "https://www.python.org/"
+
 loader = WebBaseLoader(url)
 
 docs = loader.load()
 
-print(len(docs))
-print(docs[0].page_content)
+
+prompt = PromptTemplate(
+    template="Answer the following questions \n {question} from the follwoing text \n {text}",
+    input_variables=['question','text']
+)
+
+chain = prompt | model | parser
+
+result = chain.invoke({'question': 'What is the python?','text':docs})
+
+print(result)
